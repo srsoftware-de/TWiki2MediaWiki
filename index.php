071c0d5822be60b90d257a97ea0e9dd79dff41fd
Stephan Richter <srichter@srsoftware.de>
2014-08-11 11:27:42 +0200

    new file:   index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+
+
+
+<html>
+  <title>TWiki to Mediawiki converter</title>
+<head>
+</head>
+<body>
+  <div class="upper left half"></div>
+  <div class="upper right half"></div>
+  <div class="lower left half"></div>
+  <div class="lower right half"></div>
+</body>
