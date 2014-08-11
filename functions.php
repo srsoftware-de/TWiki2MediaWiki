83edd8eb7ce9c7312b0beb146db296a988b22c6b
Stephan Richter <srichter@srsoftware.de>
2014-08-11 11:48:15 +0200

    functions.php added

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+  function ask_for_source(){
+    global $upperleft;
+    $upperleft='<form method="POST" action=".">
+      <input type="text" name="sourcewiki" /> Quell-Wiki
+    </form>';
+  
+  }
+?>
--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php
-  include_once init.php;
+  include_once "init.php";
+  include_once "functions.php";
 
   if (!isset($_SESSION['target_base'])) {
-
+    ask_for_source();
   }
 
 ?>
 
 
 <html>
-  <title>TWiki to Mediawiki converter</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
-<head>
-</head>
-<body>
-  <div class="upper left half"><?php print $upperleft;?></div>
-  <div class="upper right half"><?php print $upperright; ?></div>
-  <div class="lower left half"><?php print $lowerleft; ?></div>
-  <div class="lower right half"><?php print $lowerright; ?></div>
-</body>
+  <head>
+    <title>TWiki to Mediawiki converter</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class="upper left half"><?php print $upperleft;?></div>
+    <div class="upper right half"><?php print $upperright; ?></div>
+    <div class="lower left half"><?php print $lowerleft; ?></div>
+    <div class="lower right half"><?php print $lowerright; ?></div>
+  </body>
+</html>
